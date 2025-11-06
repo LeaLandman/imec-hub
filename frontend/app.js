1277ba21d5c70dc9c5c4eb3f9b4d81d3c04086f6
LeaLandman <lea.landman@gmail.com>
2025-11-06 10:52:27 +0100

    Delete frontend/app.js

--- frontend/app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const headline = document.getElementById("headline");
-  headline.textContent = "Bienvenue sur IMEC Intelligence Hub ✨";
-});
